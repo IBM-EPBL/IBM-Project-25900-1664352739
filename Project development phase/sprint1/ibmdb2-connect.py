b43c02211a6d03881d95800ad121a202c8e7655c
nithishkumar224 <nith19128.it@rmkec.ac.in>
2022-11-17 17:02:20 +0530

    Sprint1

---
+++ Project development phase/sprint1/ibmdb2-connect.py
@@ -0,0 +1,5 @@
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31864;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=pgc34284;PWD=AqmC4iEvLyyQvZBj",'','')
+print(conn)
+print("connection successful")
+
